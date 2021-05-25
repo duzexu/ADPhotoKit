ee8e54bba11d49627cdee608329663e5bfdf87ee
xu <zexu007@qq.com>
2021-05-25 15:57:33 +0800

    spm更新

--- Package.swift
+++ Package.swift
@@ -16,18 +16,17 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "6.0.0"),
-        .package(url: "https://github.com/onevcat/Kingfisher.git"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.0.0"),
     ],
-    
     
     targets: [
         .target(
             name: "ADPhotoKit",
-            path: "ADPhotoKit/Classes"
+            path: "ADPhotoKit/Classes",
             resources: [
                 .process("ADPhotoKit/Assets")
-            ]
+            ],
             dependencies: [
                 "SnapKit","Kingfisher"
         ]),
